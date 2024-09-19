8bbf68cc532f31c8d9798e62db1d2a87b640bccf
Ola Lindberg <ola.lindberg@ntig.se>
2024-09-19 10:02:30 +0200

    indentering

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 class App < Sinatra::Base
 
-    # Funktion som returnerar en databaskoppling
+    # Funktion för att prata med databasen
     # Exempel på användning: db.execute('SELECT * FROM fruits')
     def db
-        return @db if @db
+      return @db if @db
 
-        @db = SQLite3::Database.new("db/fruits.sqlite")
-        @db.results_as_hash = true
+      @db = SQLite3::Database.new("db/fruits.sqlite")
+      @db.results_as_hash = true
 
-        return @db
+      return @db
     end
 
     # Routen gör en redirect till '/fruits'
@@ -18,31 +18,32 @@ class App < Sinatra::Base
 
     #Routen hämtar alla frukter i databasen
     get '/fruits' do
-        @fruits = db.execute('SELECT * FROM fruits')
-        p @fruits
-        erb(:"fruits/index")
+      @fruits = db.execute('SELECT * FROM fruits')
+      p @fruits
+      erb(:"fruits/index")
     end
 
     # Routen visar ett formulär för att spara en ny frukt till databasen.
     get '/fruits/new' do
-        erb(:"fruits/new")
+      erb(:"fruits/new")
     end
 
     # Routen sparar en frukt till databasen och gör en redirect till '/fruits'.
     post '/fruits' do
+      p params
       name = params["fruit_name"]
       description = params["fruit_description"]
 
-	    db.execute("INSERT INTO fruits (name, description) VALUES(?,?)", [name, description])
+      db.execute("INSERT INTO fruits (name, description) VALUES(?,?)", [name, description])
 
-			redirect("/fruits")
+      redirect("/fruits")
     end
 
     # Övning no. 1
     # Routen visar all info (från databasen) om en frukt.
     get '/fruits/:id' do | id |
-        #todo hämta data från databasen för frukten. Använd id.
-        erb(:"fruits/show")
+      #todo hämta data från databasen för frukten med id
+      erb(:"fruits/show")
     end
 
     #Routen tar bort frukten med @id
